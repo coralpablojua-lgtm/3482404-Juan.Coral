e5c3f909f0aee0a3c794c6ff937bba1866dabaf4
Juan Coral <coralpablojua@gmail.com>
2026-08-04 14:24:17 -0500

    Ejercicio2: (promedios y estados del curso) este ejercicio esta en proceso

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -1,1 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class PromedioCurso {
+
+    public static void main(String[] args) {
+
+        int n = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántos estudiantes hay?"));
+
+        double[] notas = new double[n];
+
+        double suma = 0;
+        double promedio;
+
+    }
+}
 
