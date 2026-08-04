66f81a80821ab3030b8508fa7826dee69f027175
Juan Coral <coralpablojua@gmail.com>
2026-08-04 14:50:10 -0500

    Ejercicio3: (Mayor y menor del array) el ejercicio fue finalizado con exito

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -4,22 +4,17 @@ public class Ejercicio3 {
 
     public static void main(String[] args) {
 
-        // Pedir la cantidad de números
         int n = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántos números desea ingresar?"));
 
-        // Crear el arreglo
         int[] numeros = new int[n];
 
-        // Ingresar los números
         for (int i = 0; i < n; i++) {
             numeros[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número " + (i + 1) + ":"));
         }
 
-        // Inicializar el mayor y el menor con el primer número
         int mayor = numeros[0];
         int menor = numeros[0];
 
-        // Recorrer el arreglo para encontrar el mayor y el menor
         for (int i = 1; i < n; i++) {
 
             if (numeros[i] > mayor) {
@@ -31,9 +26,6 @@ public class Ejercicio3 {
             }
         }
 
-        // Mostrar resultados
-        JOptionPane.showMessageDialog(null,
-                "El número mayor es: " + mayor +
-                        "\nEl número menor es: " + menor);
+        JOptionPane.showMessageDialog(null, "El número mayor es: " + mayor + "\nEl número menor es: " + menor);
     }
 }
