f05ba4d2ce88b9f9a0137a4a67bd5c02493e97ba
Juan Coral <coralpablojua@gmail.com>
2026-08-06 14:22:25 -0500

    Ejercicio6 (Conteo de votos por candidato) El ejercicio fue terminado con exito

--- src/Ejercicio6.java
+++ src/Ejercicio6.java
@@ -25,7 +25,42 @@ public class Ejercicio6 {
             } while (!votos[i].equals("A") && !votos[i].equals("B") && !votos[i].equals("C"));
         }
 
+        for (int i = 0; i < n; i++) {
 
+            switch (votos[i]) {
+                case "A":
+                    votosA++;
+                    break;
+
+                case "B":
+                    votosB++;
+                    break;
+
+                case "C":
+                    votosC++;
+                    break;
+            }
+        }
+
+        String ganador;
+
+        if (votosA > votosB && votosA > votosC) {
+            ganador = "El ganador es el candidato A.";
+        } else if (votosB > votosA && votosB > votosC) {
+            ganador = "El ganador es el candidato B.";
+        } else if (votosC > votosA && votosC > votosB) {
+            ganador = "El ganador es el candidato C.";
+        } else {
+            ganador = "Hay un empate entre los candidatos con mayor número de votos.";
+        }
+
+        String resultado = "RESULTADOS DE LA VOTACIÓN\n\n";
+        resultado += "Candidato A: " + votosA + " votos\n";
+        resultado += "Candidato B: " + votosB + " votos\n";
+        resultado += "Candidato C: " + votosC + " votos\n\n";
+        resultado += ganador;
+
+        JOptionPane.showMessageDialog(null, resultado);
 
     }
 }
