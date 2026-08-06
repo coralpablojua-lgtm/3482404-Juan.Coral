658b8dc8e387aeb274e530d6c8e7ce42f3483205
Juan Coral <coralpablojua@gmail.com>
2026-08-06 14:13:27 -0500

    Ejercicio6 (Conteo de votos por candidato) El ejercicio va en proceso

--- src/Ejercicio6.java
+++ src/Ejercicio6.java
@@ -1,5 +1,31 @@
+import javax.swing.*;
+
 public class Ejercicio6 {
     public static void main(String[] args){
+
+        int n = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántos votos se van a registrar?"));
+
+        String[] votos = new String[n];
+
+        int votosA = 0;
+        int votosB = 0;
+        int votosC = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            do {
+                votos[i] = JOptionPane.showInputDialog(
+                        "Ingrese el voto " + (i + 1) + " (A, B o C):").toUpperCase();
+
+                if (!votos[i].equals("A") && !votos[i].equals("B") && !votos[i].equals("C")) {
+                    JOptionPane.showMessageDialog(null,
+                            "Voto inválido. Solo puede ingresar A, B o C.");
+                }
+
+            } while (!votos[i].equals("A") && !votos[i].equals("B") && !votos[i].equals("C"));
+        }
+
+
 
     }
 }
